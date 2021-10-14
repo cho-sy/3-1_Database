a77c8dc6df5b32ecce20bfc3aac9f2ef2f9daa6c
cho-sy <62742269+cho-sy@users.noreply.github.com>
2021-10-14 11:14:59 +0900

    Create procedure.sql

---
+++ procedure.sql
@@ -0,0 +1,13 @@
+use madang;
+delimiter //
+create procedure InsertBook(
+	in myBookID integer,
+    in myBookName varchar(40),
+    in myPublisher varchar(40),
+    in myPrice integer)
+begin
+	insert into Book(bookid, bookname, publisher, price)
+    values(myBookID, myBookName, myPublisher, myPrice);
+END;
+//
+delimiter ;  
