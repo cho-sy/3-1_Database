42c88dcec304ce8f21f1cc273f4ee51c52a69be0
cho-sy <62742269+cho-sy@users.noreply.github.com>
2021-11-12 11:21:37 +0900

    Update 정규화 이상현상.sql

--- 정규화 이상현상.sql
+++ 정규화 이상현상.sql
@@ -9,4 +9,30 @@ insert into summer values(150, 'PASCAL', 15000);
 insert into summer values(200, 'C', 10000);
 insert into summer values(250, 'FORTRAN', 20000);
 
+select sid,class from summer;
+select price from summer where class like 'C';
+select distinct class from summer where price = (select max(price) from summer);
+select count(*) , sum(price) from summer;
+
+#삭제이상
+#7-1
+select price as 'C 수강료' from summer where class like 'C';
+delete from summer where sid=200;
+insert into summer values(200, 'C', 10000);
+
+#삽입이상
+insert into summer values(null, 'Java', 25000);
 select * from summer;
+select count(*) '수강인원' from summer; # 5
+select count(sid) '수강인원' from summer; #위 값과 다름 4
+select count(*) '수강인원' from summer where sid is not null; # 4
+
+#수정이상
+update summer set price = 15000 where class = 'FORTRAN';
+select * from summer;
+select distinct price 'Fortran 수강료' from summer where class like 'FORTRAN';
+update summer set price = 20000 where class =  'FORTRAN';
+update summer set price = 15000 where class = 'FORTRAN' and sid =100;
+
+
+
